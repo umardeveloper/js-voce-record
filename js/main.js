2ea5e8f173d697983ad52293456e67d5802f87ea
umardeveloper <umar.abduganiyev@yandex.ru>
2021-09-28 12:10:42 -0700

    umar js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var rec = new webkitSpeechRecognition();
+var contain = document.querySelector('.container')
+
+rec.lang = 'uz-UZ';
+
+
+rec.onresult = function (evt) {
+	var recvoice = evt.results[0][0].transcript;
+	Element.innerHtml = null;
+
+
+	if (recvoice.includes('1') ) {
+		var newIframe = document.createElement('iframe');
+		newIframe.setAttribute('src' , 'https://www.youtube.com/embed/VdcrHd_BdNc?list=PLwsopmzfbOn_1XLqRYV7gu8ozcYvPEqmd')
+		newIframe.textContent = recvoice;
+		contain.appendChild(newIframe);
+	} 	
+	if (recvoice.includes('2') ) {
+		var newIframe = document.createElement('iframe');
+		newIframe.setAttribute('src' , 'https://www.youtube.com/embed/QUMzEd8XCkI')
+		newIframe.textContent = recvoice;
+		contain.appendChild(newIframe);
+	} 
+	if (recvoice.includes('3') ) {
+		var newIframe = document.createElement('iframe');
+		newIframe.setAttribute('src' , 'https://www.youtube.com/embed/mA_O7onmKQ8?list=PL8O4u0nxMURsQZHX2G8B3ttbhvRzxJ3md')
+		newIframe.textContent = recvoice;
+		contain.appendChild(newIframe);
+	} 
+	
+	
+
+};
+
+
+recBtn.addEventListener('click', function () {
+	console.log('==== Aloqa boshlandi ====');
+	rec.start();
+});
+
+// Asadbek aka o'zm qldm ko'chrmadm
+
+
